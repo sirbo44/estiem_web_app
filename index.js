018ee2f58d2d63d739c8fa8460d5c9d0b4287b4c
Syrmpopoulos Stavros <stsirbp@gmail.com>
2025-07-01 04:27:52 +0300

    Checkin Checkout Error Success implementations

--- index.js
+++ index.js
@@ -5,11 +5,37 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded());
 
+const QRcodeKey = 'Hey guys this is my QR code';
+const location = [37.9417, 23.6532]
+// const location =[38.0248, 23.7368]
+
 
 app.get('/', (req, res) => {
-    res.render('pages/home');
+    res.render('pages/home', {title:'Home Page'});
 });
 
+app.get('/checkin', (req, res) => {
+    res.render('pages/checkin', {title:'Check In Page'});
+})
+
+app.post('/checkin', (req, res) => {
+    console.log(req.body);
+    console.log('Check-in successful for:', req.body.firstname+" "+req.body.lastname);
+    res.redirect('/');
+    // BEFORE REDIRECT PRINT A SUCCESS MESSAGE
+});
+
+app.get('/checkout', (req, res) => {
+    res.render('pages/checkout', {title:'Check Out Page', QRcodeKey: QRcodeKey, location: location});
+})
+
+app.get('/success', (req, res) => {
+    res.render('pages/success', {title:'Success Page'});
+});
+
+app.get('/error', (req, res) => {
+    res.render('pages/error', {title:'Error Page'});
+});
 
 app.listen(port, () => {
     console.log('Server is running on http://localhost: '+ port);
